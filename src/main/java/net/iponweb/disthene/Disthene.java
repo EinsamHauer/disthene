1bf1b75e1195ef0e6e7a9284245f976c20615952
aivanov <andrei.b.ivanov@gmail.com>
2014-10-20 22:14:23 +0400

    initial

---
+++ src/main/java/net/iponweb/disthene/Disthene.java
@@ -0,0 +1,10 @@
+package net.iponweb.disthene;
+
+/**
+ * Hello world!
+ */
+public class Disthene {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
