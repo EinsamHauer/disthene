efdf6e7a36f19e438ad2f1aa1803174b3b82b9cc
Andrei Ivanov <Andrei.B.Ivanov@gmail.com>
2015-05-29 01:32:42 +0300

    rolled back experimental stuff

--- src/main/java/net/iponweb/disthene/service/store/BatchMetricProcessor.java
+++ src/main/java/net/iponweb/disthene/service/store/BatchMetricProcessor.java
@@ -6,6 +6,7 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.RateLimiter;
 import net.engio.mbassy.bus.MBassador;
+import net.iponweb.disthene.Disthene;
 import net.iponweb.disthene.bean.Metric;
 import net.iponweb.disthene.events.DistheneEvent;
 import net.iponweb.disthene.events.StoreErrorEvent;
@@ -37,11 +38,13 @@ public class BatchMetricProcessor {
     private MBassador<DistheneEvent> bus;
     private RateLimiter rateLimiter;
 
-    private final Executor executor = MoreExecutors.listeningDecorator(Executors.newCachedThreadPool());
-    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1, new NameThreadFactory(SCHEDULER_NAME));
+    private final ThreadPoolExecutor tpExecutor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+    private final Executor executor = MoreExecutors.listeningDecorator(tpExecutor);
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1, new NameThreadFactory(SCHEDULER_NAME));
+    private ScheduledExecutorService statsScheduler = Executors.newScheduledThreadPool(1, new NameThreadFactory(SCHEDULER_NAME));
 
 
-    public BatchMetricProcessor(Session session, int batchSize, int flushInterval, int maxThroughput, MBassador<DistheneEvent> bus) {
+    public BatchMetricProcessor(final Session session, final int batchSize, int flushInterval, int maxThroughput, final MBassador<DistheneEvent> bus) {
         this.session = session;
         this.batchSize = batchSize;
         this.bus = bus;
@@ -50,18 +53,83 @@ public class BatchMetricProcessor {
 
         rateLimiter = RateLimiter.create(maxThroughput / (60 * batchSize));
 
+        //todo: remove debug
+        statsScheduler.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                logger.debug("****Threads in the pool: " + tpExecutor.getPoolSize());
+                logger.debug("****Bus has pending messages: " + Disthene.dispatch.getPendingMessages().size());
+                logger.debug("****Metrics in queue: " + metrics.size());
+            }
+        }, 0, 30, TimeUnit.SECONDS);
+
+/*
         scheduler.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
                 flush();
             }
-        }, flushInterval, flushInterval, TimeUnit.SECONDS);
+        }, 100, 100, TimeUnit.MILLISECONDS);
+*/
+
+        ThreadFactory flusherFactory =  new NameThreadFactory("distheneCassandraWriters");
+        for (int i = 0; i < 10; i++) {
+            Thread flusher = flusherFactory.newThread(new Runnable() {
+                @Override
+                public void run() {
+                    int currentBatchSize = 0;
+                    BatchStatement batch = new BatchStatement();
+
+                    while (true) {
+                        Metric metric = metrics.poll();
+
+                        if (metric != null) {
+                            batch.add(
+                                    statement.bind(
+                                            metric.getRollup() * metric.getPeriod(),
+                                            Collections.singletonList(metric.getValue()),
+                                            metric.getTenant(),
+                                            metric.getRollup(),
+                                            metric.getPeriod(),
+                                            metric.getPath(),
+                                            metric.getUnixTimestamp()
+                                    )
+                            );
+                            currentBatchSize++;
+
+                            if (currentBatchSize == batchSize) {
+                                ResultSetFuture future = session.executeAsync(batch);
+
+                                Futures.addCallback(future,
+                                        new FutureCallback<ResultSet>() {
+                                            @Override
+                                            public void onSuccess(ResultSet result) {
+                                            }
+
+                                            @Override
+                                            public void onFailure(Throwable t) {
+                                                logger.error(t);
+                                            }
+                                        },
+                                        executor
+                                );
+
+                                currentBatchSize = 0;
+                                batch = new BatchStatement();
+                            }
+                        }
+                    }
+                }
+            });
+
+            flusher.start();
+        }
     }
 
 
     public void add(Metric metric) {
-        metrics.add(metric);
-        executeIfNeeded();
+        metrics.offer(metric);
+//        executeIfNeeded();
     }
 
     private void executeIfNeeded() {
@@ -74,19 +142,18 @@ public class BatchMetricProcessor {
 
     private void flush() {
         if (metrics.size() > 0) {
-            if (executing.compareAndSet(false, true)) {
                 execute(batchSize);
-            }
         }
     }
 
     private synchronized void execute(int minBatchSize) {
+
         while (metrics.size() >= minBatchSize) {
             int currentBatchSize = 0;
             final BatchStatement batch = new BatchStatement();
 
             while (currentBatchSize < batchSize && metrics.size() > 0) {
-                Metric metric = metrics.remove();
+                Metric metric = metrics.poll();
                 batch.add(
                     statement.bind(
                             metric.getRollup() * metric.getPeriod(),
@@ -113,12 +180,12 @@ public class BatchMetricProcessor {
                     new FutureCallback<ResultSet>() {
                         @Override
                         public void onSuccess(ResultSet result) {
-                            bus.post(new StoreSuccessEvent(finalCurrentBatchSize)).asynchronously();
+//                            bus.post(new StoreSuccessEvent(finalCurrentBatchSize)).asynchronously();
                         }
 
                         @Override
                         public void onFailure(Throwable t) {
-                            bus.post(new StoreErrorEvent(finalCurrentBatchSize)).asynchronously();
+//                            bus.post(new StoreErrorEvent(finalCurrentBatchSize)).asynchronously();
                             logger.error(t);
                             // let's also penalize an error by additionally throttling C* writes
                             // effectively let's suspend it for like 10 seconds hoping C* will recover after that
@@ -132,8 +199,8 @@ public class BatchMetricProcessor {
         executing.set(false);
     }
 
-
     public void shutdown() {
         scheduler.shutdown();
     }
+
 }
