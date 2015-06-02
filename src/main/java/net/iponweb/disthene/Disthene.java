4155e049d289e6a77f0c07abaa5b0218decbad10
Andrei Ivanov <Andrei.B.Ivanov@gmail.com>
2015-06-03 01:52:26 +0300

    enabled ES disabled C*

--- src/main/java/net/iponweb/disthene/Disthene.java
+++ src/main/java/net/iponweb/disthene/Disthene.java
@@ -100,11 +100,11 @@ public class Disthene {
             logger.info("Creating stats");
             statsService = new StatsService(bus, distheneConfiguration.getStats(), distheneConfiguration.getCarbon().getBaseRollup());
 
-//            logger.info("Creating ES index service");
-//            indexService = new IndexService(distheneConfiguration.getIndex(), bus);
+            logger.info("Creating ES index service");
+            indexService = new IndexService(distheneConfiguration.getIndex(), bus);
 
-            logger.info("Creating C* service");
-            cassandraService = new CassandraService(distheneConfiguration.getStore(), bus);
+//            logger.info("Creating C* service");
+//            cassandraService = new CassandraService(distheneConfiguration.getStore(), bus);
 
             logger.info("Loading aggregation rules");
             in = Files.newInputStream(Paths.get(aggregationConfigLocation));
@@ -220,11 +220,11 @@ public class Disthene {
             bus.shutdown();
 
             // Now flush what's left and shutdown
-//            logger.info("Shutting down ES service");
-//            indexService.shutdown();
+            logger.info("Shutting down ES service");
+            indexService.shutdown();
 
-            logger.info("Shutting down C* service");
-            cassandraService.shutdown();
+//            logger.info("Shutting down C* service");
+//            cassandraService.shutdown();
 
             logger.info("Shutdown complete");
 
