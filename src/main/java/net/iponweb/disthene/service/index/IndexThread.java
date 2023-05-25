e7ceae52564788a999e6362ba6fb0079b649e6bb
Robert Jastrzebski <Robert.Jastrzebski@ringieraxelspringer.pl>
2023-05-25 09:07:32 +0000

    OS mget

--- src/main/java/net/iponweb/disthene/service/index/IndexThread.java
+++ src/main/java/net/iponweb/disthene/service/index/IndexThread.java
@@ -1,20 +1,32 @@
 package net.iponweb.disthene.service.index;
 
 import net.iponweb.disthene.bean.Metric;
+import net.iponweb.disthene.bean.MetricPath;
+
+import org.apache.http.cookie.SetCookie;
 import org.apache.log4j.Logger;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch._types.Refresh;
 import org.opensearch.client.opensearch.core.BulkRequest;
 import org.opensearch.client.opensearch.core.BulkResponse;
+import org.opensearch.client.opensearch.core.MgetRequest;
+import org.opensearch.client.opensearch.core.MgetResponse;
 import org.opensearch.client.opensearch.core.bulk.BulkOperation;
-import org.opensearch.client.opensearch.core.bulk.BulkResponseItem;
 import org.opensearch.client.opensearch.core.bulk.IndexOperation;
+import org.opensearch.client.opensearch.core.mget.MultiGetOperation;
+import org.opensearch.client.opensearch.core.mget.MultiGetResponseItem;
+import org.opensearch.client.opensearch.core.mget.MultiGetResponseItemBuilders;
+import org.opensearch.client.opensearch.core.mget.MultiGetError.Builder;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.TreeMap;
+
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -31,8 +43,9 @@ public class IndexThread extends Thread {
     private int flushInterval;
     protected volatile boolean shutdown = false;
     protected Queue<Metric> metrics;
-    ArrayList<Metric> requests = new ArrayList<Metric>();
+    Map<String, Metric> requests = new HashMap<>();
     private long lastFlushTimestamp = System.currentTimeMillis() / 1000L;
+    private List mGetDocuments;
 
     public IndexThread(String name, OpenSearchClient client, Queue<Metric> metrics, String index, String type, int batchSize, int flushInterval) {
         super(name);
@@ -65,7 +78,8 @@ public class IndexThread extends Thread {
     }
 
     private void addToBatch(Metric metric) {
-        requests.add(metric);
+        
+        requests.put(this.hashPath(metric.getPath()), metric); ///add(metric);
         if (requests.size() >= batchSize || (lastFlushTimestamp < System.currentTimeMillis() / 1000L - flushInterval)) {
             flush();
             lastFlushTimestamp = System.currentTimeMillis() / 1000L;
@@ -73,25 +87,44 @@ public class IndexThread extends Thread {
     }
 
     private void flush() {
-        ArrayList<BulkOperation> ops = new ArrayList<>();
-        for (Metric m : requests) {
-            final String[] parts = m.getPath().split("\\.");
-            final StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < parts.length; i++) {
-                if (sb.toString().length() > 0) {
-                    sb.append(".");
-                }
-                sb.append(parts[i]);
-                String path = sb.toString();
-                final Map<String, Object> record = new TreeMap<>();
-                record.put("path", path);
-                record.put("depth", i + 1);
-                record.put("tenant", m.getTenant());
-                record.put("leaf", (i == parts.length - 1));
+        final Set<String> ids = requests.keySet();
+        MgetRequest mgetreq = new MgetRequest.Builder()
+                .index(index)
+                .ids(new ArrayList<>(ids))
+                .refresh(true)
+                .build();
 
-                ops.add(new BulkOperation.Builder().index(
-                        IndexOperation.of(io -> io.index(index).id(hashPath(path)).document(record))
-                ).build());
+        List<MultiGetResponseItem> mGetDocuments = new ArrayList<MultiGetResponseItem>();
+        try {
+            MgetResponse mGetResponse = client.mget(mgetreq, Map.class);
+            mGetDocuments = mGetResponse.docs();
+        } catch (IOException e) {
+            logger.error(e);
+        }
+        
+        ArrayList<BulkOperation> ops = new ArrayList<>();
+        for (MultiGetResponseItem m : mGetDocuments) {
+            if(!m.result().found()) {
+                final String id = m.result().id();
+                final Metric metric = requests.get(id);
+                final String[] parts = metric.getPath().split("\\.");
+                final StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < parts.length; i++) {
+                    if (sb.toString().length() > 0) {
+                        sb.append(".");
+                    }
+                    sb.append(parts[i]);
+                    String path = sb.toString();
+                    final Map<String, Object> record = new TreeMap<>();
+                    record.put("path", path);
+                    record.put("depth", i + 1);
+                    record.put("tenant", metric.getTenant());
+                    record.put("leaf", (i == parts.length - 1));
+
+                    ops.add(new BulkOperation.Builder().index(
+                            IndexOperation.of(io -> io.index(index).id(id).document(record))
+                    ).build());
+                }
             }
 
         }
@@ -107,7 +140,11 @@ public class IndexThread extends Thread {
         } catch (IOException e) {
             logger.error(e);
         }
-        requests = new ArrayList<Metric>();
+        requests = new HashMap<String, Metric>();
+    }
+
+    public void shutdown() {
+        shutdown = true;
     }
 
     private String hashPath(String path) {
@@ -121,9 +158,5 @@ public class IndexThread extends Thread {
             logger.error("MD5: ", e);
             return "ERR";
         }
-    }
-
-    public void shutdown() {
-        shutdown = true;
     }
 }
